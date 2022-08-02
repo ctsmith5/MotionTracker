337984f2d41aa6f9a45c27dfcb561acde2967184
Colin Smith <ctsmith5@ncsu.edu>
2022-08-02 09:53:48 -0400

    Initial Commit

---
+++ MotionDemo/MotionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionDemoApp.swift
+//  MotionDemo
+//
+//  Created by Colin Smith on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MotionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
